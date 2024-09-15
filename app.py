97baa2232d0bbc3327b7abcc93480cdbb9b5459b
alexlucas03 <alexlucas03@gmail.com>
2024-09-15 16:37:37 -0700

    dish list

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import time
+import datetime
 from dish import Dish
 
 app = Flask(__name__)
@@ -7,8 +7,33 @@ app.config['SECRET_KEY'] = 'mysecret'
 active_user = None
 
 dishes = []
+startDate = "2024-09-15"
+endDate = "2024-12-08"
+start_date_obj = datetime.datetime.strptime(startDate, "%Y-%m-%d")
+end_date_obj = datetime.datetime.strptime(endDate, "%Y-%m-%d")
+
+delta = datetime.timedelta(days=1)
+current_date = start_date_obj
+
+while current_date <= end_date_obj:
+        # Create a new Dish object with the current date as an attribute
+        dish = Dish(date=current_date.strftime("%Y-%m-%d"))
+        dishes.append(dish)
+        current_date += delta
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            dishes=dishes)
+
+
+def get_day_of_week(date):
+    """Gets the day of the week for a given date string."""
+
+    # Get the day of the week as a string
+    day_of_week = date.strftime("%A")
+
+    return day_of_week
+
+if __name__ == '__main__':
+    app.run(debug=True)
