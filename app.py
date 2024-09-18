8f3225f1b7543d7ecee09e5cdbe1efb3aa1b8222
alexlucas03 <alexlucas03@gmail.com>
2024-09-17 23:14:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ for dish in dishes:
 
 for month, days in grouped_dishes.items():
     for day, day_dishes in days.items():
-        while len(day_dishes) < 4:
+        while len(day_dishes) <= 4:
             day_dishes.append(Dish(date="", owner="x", type=""))
 
 @app.route('/')
