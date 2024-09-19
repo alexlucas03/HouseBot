58d919bb16098e4b7512c43319c919e3cf1fba6a
alexlucas03 <alexlucas03@gmail.com>
2024-09-19 10:33:15 -0700

    1.0

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SECRET_KEY'] = 'mysecret'
 
 dishes = []
 total_points = 0
-start_date_str = "2024-09-19"
+start_date_str = "2024-09-24"
 end_date_str = "2024-12-13"
 start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d")
 end_date = datetime.datetime.strptime(end_date_str, "%Y-%m-%d")
