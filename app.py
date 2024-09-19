9e36bb66ef5ea7ef909276f490b9a251d6546ad6
alexlucas03 <alexlucas03@gmail.com>
2024-09-19 13:22:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -176,8 +176,8 @@ def index():
     x1_owner = today_x1.owner if today_x1 and today_x1.owner else 'Not Assigned'
 
     recalculate_points()
-    if points_order[pick_order.index(user)] <= 0:
-                logout()
+    if points_order[pick_order.index(user)] <= 1:
+        logout()
     return render_template('index.html', grouped_dishes=grouped_dishes, user=user, points_order=points_order, pick_order=pick_order)
 
 
