d6d5350d34136651f41634dd20a76468a2ac1400
alexlucas03 <alexlucas03@gmail.com>
2024-09-19 11:04:35 -0700

    fix

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def send_groupme_messages():
     x1_message = f"x1: @{x1_owner}"
     send_message(x1_message, x1_owner, x1_userid, 4, 4 + len(x1_owner))
 
-    return jsonify({'success': True, 'message': 'Messages sent successfully'})
+    return redirect(url_for('/'))
 
 @app.route('/')
 def index():
