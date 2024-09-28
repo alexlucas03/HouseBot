75381a5a85f42bab1131eefd8d72e973c6a1cd6a
alexlucas03 <alexlucas03@gmail.com>
2024-09-28 12:40:51 -0700

    Update dish.py

--- dish.py
+++ dish.py
@@ -1,11 +1,11 @@
 import datetime
 
 class Dish:
-    def __init__(self, year, month, day, dish_type, owner, id):
+    def __init__(self, year, month, day, type, owner, id):
         self.date_obj = datetime.date(year, month, day)
         self.weekday = self.date_obj.strftime("%A")
         self.date_str = self.date_obj.strftime("%Y-%m-%d")
-        self.type = dish_type
+        self.type = type
         self.owner = owner
         self.id - id
 
