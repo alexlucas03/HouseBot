2f8523f83584c5129f86336a8613e2e238031326
alexlucas03 <alexlucas03@gmail.com>
2024-11-19 19:13:27 -0800

    Update laundry.py

--- laundry.py
+++ laundry.py
@@ -1,12 +1,14 @@
 class Laundry:
-    def __init__(self, name, appliance, rank):
+    def __init__(self, name, appliance, rank, checksleft):
         self.name = name
         self.appliance = appliance
         self.rank = rank
+        self.checksleft = checksleft
 
     def to_dict(self):
         return {
             'name': self.name,
             'userID': self.appliance,
             'pickOrder': self.rank,
+            'checksleft': self.checksleft
         }
