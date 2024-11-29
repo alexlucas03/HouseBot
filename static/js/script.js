52d9bce0a0d7b8a22b8c3ba4b6163735f1a38263
alexlucas03 <alexlucas03@gmail.com>
2024-11-28 17:56:56 -0800

    move script, struct help

--- static/js/script.js
+++ static/js/script.js
@@ -47,3 +47,159 @@ function unclaim(month, id) {
     })
     .catch(error => console.error('Error:', error));
 }
+
+function checkInitial() {
+    var initialBar = document.querySelector(".initialbar");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+    var subBarChildren = subBar.children;
+    
+    for (var i = 0; i < subBarChildren.length; i++) {
+        if (subBarChildren[i] !== initialBar) {
+            subBarChildren[i].style.display = "none";
+        }
+    }
+
+    initialBar.style.display = (initialBar.style.display === "block") ? "none" : "block";
+    subBar.style.display = initialBar.style.display
+}
+
+function checkChange() {
+    var changeBody = document.querySelector(".changebody");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+
+    subBar.style.display = "none";
+    
+    var bigBodyChildren = bigBody.children;
+    for (var i = 0; i < bigBodyChildren.length; i++) {
+        if (bigBodyChildren[i] !== changeBody) {
+            bigBodyChildren[i].style.display = "none";
+        }
+    }
+
+    var subBarChildren = subBar.children;
+    for (var i = 0; i < subBarChildren.length; i++) {
+        subBarChildren[i].style.display = "none";
+    }
+
+    changeBody.style.display = (changeBody.style.display === "block") ? "none" : "block";
+}
+
+function checkManual() {
+    var manualBar = document.querySelector(".manualbar");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+    var subBarChildren = subBar.children;
+
+    for (var i = 0; i < subBarChildren.length; i++) {
+        if (subBarChildren[i] !== manualBar) {
+            subBarChildren[i].style.display = "none";
+        }
+    }
+
+    manualBar.style.display = (manualBar.style.display === "block") ? "none" : "block";
+    subBar.style.display = manualBar.style.display
+}
+
+function checkPicks() {
+    var picksBody = document.querySelector(".picksbody");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+
+    subBar.style.display = "none";
+    
+    var bigBodyChildren = bigBody.children;
+    for (var i = 0; i < bigBodyChildren.length; i++) {
+        if (bigBodyChildren[i] !== picksBody) {
+            bigBodyChildren[i].style.display = "none";
+        }
+    }
+
+    var subBarChildren = subBar.children;
+    for (var i = 0; i < subBarChildren.length; i++) {
+        subBarChildren[i].style.display = "none";
+    }
+
+    picksBody.style.display = (picksBody.style.display === "block") ? "none" : "block";
+}
+
+function checkInitialize() {
+    var initialBody = document.querySelector(".initialbody");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+
+    subBar.style.display = "none";
+    
+    var bigBodyChildren = bigBody.children;
+    for (var i = 0; i < bigBodyChildren.length; i++) {
+        if (bigBodyChildren[i] !== initialBody) {
+            bigBodyChildren[i].style.display = "none";
+        }
+    }
+
+    var subBarChildren = subBar.children;
+    for (var i = 0; i < subBarChildren.length; i++) {
+        subBarChildren[i].style.display = "none";
+    }
+
+    initialBody.style.display = (initialBody.style.display === "block") ? "none" : "block";
+}
+
+function checkRules() {
+    var rulesBody = document.querySelector(".rulesbody");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+
+    subBar.style.display = "none";
+    
+    var bigBodyChildren = bigBody.children;
+    for (var i = 0; i < bigBodyChildren.length; i++) {
+        if (bigBodyChildren[i] !== rulesBody) {
+            bigBodyChildren[i].style.display = "none";
+        }
+    }
+
+    var subBarChildren = subBar.children;
+    for (var i = 0; i < subBarChildren.length; i++) {
+        subBarChildren[i].style.display = "none";
+    }
+
+    rulesBody.style.display = (rulesBody.style.display === "block") ? "none" : "block";
+}
+
+function checkAll() {
+    var allBody = document.querySelector(".allbody");
+    var subBar = document.querySelector(".subbar");
+    var bigBody = document.querySelector(".bigbody");
+
+    subBar.style.display = "none";
+    
+    var bigBodyChildren = bigBody.children;
+    for (var i = 0; i < bigBodyChildren.length; i++) {
+        if (bigBodyChildren[i] !== allBody) {
+            bigBodyChildren[i].style.display = "none";
+        }
+    }
+
+    var subBarChildren = subBar.children;
+    for (var i = 0; i < subBarChildren.length; i++) {
+        subBarChildren[i].style.display = "none";
+    }
+
+    allBody.style.display = (allBody.style.display === "block") ? "none" : "block";
+}
+
+const addRowButton = document.querySelector('.add-row-button');
+const peopleTableBody = document.getElementById('people-table-body');
+
+addRowButton.addEventListener('click', () => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td><input type="text" name="name[]"></td>
+        <td><input type="text" name="userid[]"></td>
+    `;
+    peopleTableBody.appendChild(newRow);
+});
+
+const user = "{{ user }}";
